5dfb4b64227ca66ce4214b0953838471870a8c14
mahedi205339 <mahedi205339gmaol.com>
2023-10-19 23:43:21 +0600

    added data delete and update porstion

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectId } = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -26,13 +27,40 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+
+        // data collection 
         const carsCollection = client.db('carsDB').collection('cars');
 
+        //data created
         app.post('/cars', async (req, res) => {
             const newCar = req.body;
             console.log(newCar)
             const result = await carsCollection.insertOne(newCar);
             res.send(result)
+        })
+        //data read 
+        app.get('/cars', async (req, res) => {
+            const cursor = carsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        //data delete 
+        app.delete('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await carsCollection.deleteOne(query);
+            res.send(result)
+
+        })
+        //update data 
+        app.get('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await carsCollection.findOne(query);
+            res.send(result)
+
         })
 
         // Send a ping to confirm a successful connection
