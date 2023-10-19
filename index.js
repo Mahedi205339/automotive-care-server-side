90303b63a86886977b3564d48d761b7fea5b66c1
mahedi205339 <mahedi205339gmaol.com>
2023-10-19 21:41:44 +0600

    data succsessfully created

--- index.js
+++ index.js
@@ -8,12 +8,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-//automotiveServer
-//v2li8PwyzaYUJili
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0r9jhzc.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,17 +20,27 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
 async function run() {
     try {
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const carsCollection = client.db('carsDB').collection('cars');
+
+        app.post('/cars', async (req, res) => {
+            const newCar = req.body;
+            console.log(newCar)
+            const result = await carsCollection.insertOne(newCar);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
